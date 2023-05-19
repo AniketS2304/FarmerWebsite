88708e9ae89316e35647bc3ee01d76d7a3e10fe8
AniketS2304 <119687574+AniketS2304@users.noreply.github.com>
2023-05-19 23:22:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    var menu = document.querySelector('.menu');
+    var hamburger = document.querySelector('.hamburger');
+    hamburger.classList.toggle('active');
+    menu.classList.toggle('active');
+  }
+  
